31e452791939c7690955898a2bb82ebeec7720dc
anamiir2-collab <anamiir2@gmail.com>
2026-09-22 06:48:08 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+<!-- PWA Icons -->
+<link
+  rel="icon"
+  type="image/png"
+  sizes="32x32"
+  href="assets/icons/favicon-32.png"
+/>
+
+<link
+  rel="icon"
+  type="image/png"
+  sizes="192x192"
+  href="assets/icons/icon-192.png"
+/>
+
+<link
+  rel="icon"
+  type="image/png"
+  sizes="512x512"
+  href="assets/icons/icon-512.png"
+/>
+
+<link
+  rel="apple-touch-icon"
+  sizes="180x180"
+  href="assets/icons/apple-touch-icon.png"
+/>
+
+<meta
+  name="msapplication-TileImage"
+  content="assets/icons/icon-192.png"
+/>
+
+<meta
+  name="theme-color"
+  content="#0066FF"
+/>
